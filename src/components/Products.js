2e1f162cc09a6d993184b8769eefd25413681221
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-08-28 16:11:20 +0530

    [Add] Logic complete for filtering products by price and brand.

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,10 +4,17 @@ import ProductCard from './ProductCard';
 
 const Products = () => {
   const products = useCommonStore((state) => state.products);
+  let filteredProducts = useCommonStore((state) => state.filteredProducts);
+
+  const NoProducts = <div>No Results found , Please try using filters</div>;
+
+  if (!filteredProducts) {
+    return NoProducts;
+  }
 
   return (
     <div className="grid md:grid-cols-4  sm:grid-cols-2 gap-8">
-      {products.map((prod) => (
+      {(filteredProducts.length === 0 ? products : filteredProducts).map((prod) => (
         <ProductCard key={prod.id} prod={prod} />
       ))}
     </div>
--- src/store.js
+++ src/store.js
@@ -4,10 +4,61 @@ import { getProducts } from './api';
 const useCommonStore = create((set) => ({
   searchTerm: '',
   products: [],
+  filteredProducts: [],
   setSearchTerm: (searchTerm) => set((state) => (state.searchTerm = searchTerm)),
   fetchData: async function () {
     const data = await getProducts();
     set((state) => (state.products = data));
+  },
+  filter: {
+    priceFilter: [],
+    brandsFilter: [],
+  },
+
+  setFilter: (filterObj) => set((state) => (state.filter = { ...state.filter, ...filterObj })),
+
+  performGlobalFilter: function () {
+    set((state) => {
+      state.filteredProducts = [];
+      let filterApplied = false;
+
+      let oldProducts = state.products;
+      if (state.filter.brandsFilter.length) {
+        oldProducts.forEach((item) => {
+          if (state.filter.brandsFilter.includes(item.brand)) {
+            if (state.filter.brandsFilter.includes(item.brand)) {
+              state.filteredProducts = [...state.filteredProducts, item];
+            }
+          }
+        });
+        filterApplied = true;
+      }
+
+      if (state.filter.priceFilter.length) {
+        oldProducts = state.filteredProducts.length === 0 ? state.products : state.filteredProducts;
+        let productsFilterPrice_Brand = [];
+        for (let i = 0; i < state.filter.priceFilter.length; i += 2) {
+          oldProducts.forEach((item) => {
+            if (
+              item.price >= state.filter.priceFilter[i] &&
+              item.price <= state.filter.priceFilter[i + 1]
+            ) {
+              productsFilterPrice_Brand.push(item);
+            }
+          });
+        }
+        state.filteredProducts = productsFilterPrice_Brand;
+        filterApplied = true;
+      }
+
+      if (filterApplied === true && state.filteredProducts.length === 0) {
+        state.filteredProducts = null;
+      }
+
+      // console.log(state.unique);
+
+      // state.filteredProducts.filter((item) => state.filter.brandsFilter.includes(item.brand)),
+    });
   },
 }));
 
